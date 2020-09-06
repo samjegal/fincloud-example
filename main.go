98aeeb38346d1771b4a028f8eb79210328a92f85
samjegal <samjegal@gmail.com>
2020-09-06 20:27:42 +0900

    Add sender using hashcorp helpers

--- main.go
+++ main.go
@@ -4,16 +4,17 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/hashicorp/go-azure-helpers/sender"
 	"github.com/samjegal/fincloud-sdk-for-go/services/vpc"
 )
 
 func main() {
-	// sender := sender.BuildSender("FINCLOUD")
+	sender := sender.BuildSender("FINCLOUD")
 	client := vpc.NewClient()
 
 	client.AccessKey = "9FF16139561D9ED451BA"
 	client.Secretkey = "807D6963AD48E28745DAAD7F846AA3DFE6F27C0B"
-	// client.Sender = sender
+	client.Sender = sender
 
 	ctx := context.Background()
 	resp, err := client.GetList(ctx, "", "", "", "")
