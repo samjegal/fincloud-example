02625122d0965bb582ae37cff966e7d27f36b4eb
samjegal <samjegal@gmail.com>
2020-09-08 10:36:17 +0900

    Remove region code parameter

--- main.go
+++ main.go
@@ -14,10 +14,11 @@ func main() {
 
 	client.AccessKey = "9FF16139561D9ED451BA"
 	client.Secretkey = "807D6963AD48E28745DAAD7F846AA3DFE6F27C0B"
+
 	client.Sender = sender
 
 	ctx := context.Background()
-	resp, err := client.GetList(ctx, "", "", "", "")
+	resp, err := client.GetList(ctx, "", "", "")
 	if err != nil {
 		panic("send request faild...")
 	}
